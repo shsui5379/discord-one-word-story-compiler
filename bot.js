008a9efe0a3bc443cb1b55763da4dcc83e5a20dc
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2020-02-17 19:26:46 -0500

    no longer necessary to keep sending !continue

--- bot.js
+++ bot.js
@@ -16,19 +16,52 @@ client.on('ready', () => {
   });
 
 
+function cont(container, outlcl) { //container collects the words, outlcl is the msg sent to use to find target channel/guild
+    if (container[0][0] == TOP)  //check if there's more words
+    { //if not
+        var comp = "";
+        for (let i = 0; i < container.length; i++) {
+            if (comp.length + container[i][1].content.length >= 2000)  //split messages because 2000 character limit
+            {
+                outlcl.channel.send(comp);
+                comp = '';
+            }
+            if (!container[i][1].author.bot) //don't include bot level ups
+            {
+                comp = comp + container[i][1].content + " "; //space
+            }
+            if (i == container.length-1)
+            {
+                outlcl.channel.send(comp); //finish
+            }
+        }
+    }
+    else
+    { //if so
+        outlcl.guild.channels.get(STORYCH).fetchMessages( { before: container[0][0], limit: 100 }).then(words => { //grab the next 100 messages
+            for (var e of words)
+            {
+                container.unshift(e); //plop into the array
+            }
+            cont(container, outlcl); //keep going
+        });
+    }
+}
 client.on('message', msg => {
     if (msg.toString() == "!compile") //compiles first 100 messages
     {
-        out = [];
+        msg.channel.send("working...");
+        out = []; //holds words
         msg.guild.channels.get(STORYCH).fetchMessages( { limit: 100 }).then(words => { //grab 100 messages at a time
             for (var e of words)
             {
                 out.unshift(e); //plop into the array
             }
+            cont(out, msg); //next batch
         });
-        msg.channel.send('type `!continue` to continue');
     }
-    else if (msg.toString() == "!continue")
+    //old code
+    /*else if (msg.toString() == "!continue") 
     {
         if (out[0][0] == TOP)  //check if there's more words
         {
@@ -54,11 +87,14 @@ client.on('message', msg => {
                 msg.channel.send(comp);
                 comp = '';
             }
-            comp = comp + out[i][1].content + " "; //space
+            if (!out[i][1].author.bot)
+            {
+                comp = comp + out[i][1].content + " "; //space
+            }
             if (i == out.length-1)
             {
                 msg.channel.send(comp); //finish
             }
         }
-    }
+    }*/
 });
